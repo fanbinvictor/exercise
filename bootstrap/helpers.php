0669de4962106005c40e666177731fc8abea5692
fanbin <732155316@qq.com>
2018-06-25 16:28:19 +0800

    头部底部模板

---
+++ bootstrap/helpers.php
@@ -0,0 +1,11 @@
+<?php
+
+function test_helper()
+{
+    return 'ok';
+}
+
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
+}
---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">测试环境</p>
+        <p class="pull-right">测试环境</p>
+    </div>
+</footer>
