37445029a88c5d07ffa81e53effdcc4d365303e3
fanbin <732155316@qq.com>
2018-07-02 15:43:11 +0800

    邮件验证发送

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,12 @@ Auth::routes();
 
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
-    Route::group(['middleware' => 'email_verified'], function () {
-        Route::get('/test', function () {
-            return 'adasdsada';
-        });
-    });
+//    Route::group(['middleware' => 'email_verified'], function () {
+//        Route::get('/test', function () {
+//            return 'adasdsada';
+//        });
+//    });
+    Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
+    Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
+
 });
