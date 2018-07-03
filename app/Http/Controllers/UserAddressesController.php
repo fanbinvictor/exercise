075ba329db9b10e456e2e327c3bec5445a1cc8db
fanbin <732155316@qq.com>
2018-07-03 18:28:53 +0800

    收货地址显示

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class UserAddressesController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        return view('user_addresses.index', ['addresses' => $request->user()->addresses,]);
+    }
 }
