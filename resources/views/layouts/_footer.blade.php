64b1f68660e0dc0c613187b0d311c2a2ec7709e4
fan <732155316@qq.com>
2018-09-05 17:53:13 +0800

    底部修改

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer">
     <div class="container">
         <p class="pull-left">测试环境</p>
-        <p class="pull-right">测试环境</p>
+        <p class="pull-right"><a href="https://note.fb.wanxiaohong.cn" target="_blank">范斌的笔记</a></p>
     </div>
 </footer>
